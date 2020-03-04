10767a6a367a7999559ecca8d8fcedfbab16eb70
jyjokokk <jyrki.j.kokkola@student.jyu.fi>
2020-03-04 20:57:13 +0200

    Teoksen tarkeimmat metodit tehty.

--- src/kirjasto/Teos.java
+++ src/kirjasto/Teos.java
@@ -10,23 +10,25 @@ import java.io.*;
  */
 public class Teos {
     
-    private int id;
-    private String isbn;
-    private String nimi;
-    private String tekija;
-    private int julkaisuVuosi;
+    private int id = 0;
+    private String isbn = "";
+    private String nimi = "";
+    private String tekija = "";
+    private int julkaisuVuosi = 0;
     
-    private int seuraavaId = 1;
+    private static int seuraavaId = 1;
     
-    
+
     /**
-     * Vakiomuodostoja, joka alustaa arvot tyhjiksi.
+     * Apumetodi, jolla saadaan luotua testattava olio,
+     * annetuilla arvoilla.
+     * TODO: Lisaa randomoitu ISBN.
      */
-    public Teos() {
-        this.id = 0;
-        this.isbn = "0-000-00000-0";
-        this.tekija = "";
-        this.julkaisuVuosi = 0;
+    public void vastaaLotr() {
+        this.isbn = "978-0544003415";
+        this.nimi = "The Lord of the Rings";
+        this.tekija = "J.R.R. Tolkien";
+        this.julkaisuVuosi = 1954;
     }
     
     
@@ -36,7 +38,9 @@ public class Teos {
      * @example
      * <pre name="test">
      *     Teos t1 = new Teos();
-     *     t1.toString === "0|0-000-00000-0||0";
+     *     t1.toString() === "0||||0";
+     *     t1.vastaaLotr();
+     *     t1.toString() === "0|978-0544003415|The Lord of the Rings|J.R.R. Tolkien|1954";
      * </pre>
      */
     @Override
@@ -49,6 +53,57 @@ public class Teos {
     }
     
     
+    /**
+     * Antaa teokselle seuraavan id-numeron.
+     * @return Teoksen uusi id-numero.
+     * @example
+     * <pre name="test">
+     *  Teos t1 = new Teos();
+     *  t1.getId() === 0;
+     *  t1.rekisteroi();
+     *  t1.getId() === 1;
+     *  Teos t2 = new Teos();
+     *  t2.rekisteroi();
+     *  t2.getId() === 2;
+     *  int n1 = t1.getId();
+     *  int n2 = t2.getId();
+     *  n1 === n2 - 1;
+     * </pre>
+     */
+    public int rekisteroi() {
+        this.id = seuraavaId;
+        seuraavaId++;
+        return this.id;
+    }
+    
+    
+    /**
+     * Palauttaa teoksen id-numeron.
+     * @return teoksen id
+     */
+    public int getId() {
+        return this.id;
+    }
+    
+    
+    /**
+     * Tulostetaan teoksen tiedot.
+     * @param out tietovirta johon tulostetaan. 
+     */
+    public void tulosta(PrintStream out) {
+        out.println(this.toString());
+    }
+    
+    
+    /**
+     * Tulostetaan teoksen tiedot.
+     * @param os tietovirta johon tulostetaan.
+     */
+    public void tulosta(OutputStream os) {
+        tulosta(new PrintStream(os));
+    }
+    
+    
 
     /**
      * @param args ei kaytossa
@@ -56,6 +111,13 @@ public class Teos {
     public static void main(String[] args) {
         Teos t1 = new Teos();
         System.out.println(t1);
+        t1.vastaaLotr();
+        t1.tulosta(System.out);
+        t1.rekisteroi();
+        t1.tulosta(System.out);
+        Teos t2 = new Teos();
+        t2.rekisteroi();
+        t2.tulosta(System.out);
     
     }
 
